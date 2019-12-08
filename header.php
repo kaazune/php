f5b753107297840e556dd300af1fb3b194ec3240
大菅和音 <kaazune@gmail.com>
2019-12-09 03:03:29 +0900

    全てのファイルでインクルードする，ヘッダーフッターの共通部分のファイルを追加

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo title; ?></title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <header>
+        <div class="heada">
+            <a href="index.php"><img src="img/createvote.png"></a>
+        </div>
+        <div class="headb">
+            <ul>
+                <li><a href="#">How to</a></li>
+                <li><a href="post.php">Create</a></li>
+                <li><a href="#">Find</a></li>
+            </ul>
+        </div>
+    </header>
