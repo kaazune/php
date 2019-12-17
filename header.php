258980c2b4f4d92435f9a015e218e2c16718aa78
horiharuka <demenigisu31@gmail.com>
2019-12-18 00:53:17 +0900

    修正

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ session_start();
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo title; ?></title>
+    <title><?php echo "title"; ?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
