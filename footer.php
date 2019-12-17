99e8ef6b3d3794e99c9051d0465aaabd55fd20c4
Kazune <kaazune@gmail.com>
2019-12-18 00:40:34 +0900

    jsファイルの読み込みの順番変更

--- footer.php
+++ footer.php
@@ -6,9 +6,7 @@
 <div class="shutter shutter-a"></div>
 <div class="shutter shutter-b"></div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/barba.js/1.0.0/barba.min.js"></script>
-<script src="slick/slick.js" type="text/javascript"></script>
 <script src="script.js" type="text/javascript"></script>
-<script src="./app.js"></script>
 
 </body>
 </html>
